7f0a633a28b9de62cb209d0e9347634007dba4f8
damlayagmur <dmlygmr@windowslive.com>
2022-06-12 23:00:21 +0300

    Login Fragment added

---
+++ app/src/main/java/com/damlayagmur/moviedb/presentation/ui/login/LoginFragment.kt
@@ -0,0 +1,7 @@
+package com.damlayagmur.moviedb.presentation.ui.login
+
+import androidx.fragment.app.Fragment
+import com.damlayagmur.moviedb.R
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+}
